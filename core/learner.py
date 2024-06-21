0afc1e6245f8f33cc04d88e5950b5315dd2a3cbb
AlessioSam <alessiosampieri27@gmail.com>
2024-06-21 10:11:29 +0200

    bigger model

--- core/learner.py
+++ core/learner.py
@@ -29,8 +29,9 @@ class Learner(pl.LightningModule):
         # self.criterion = CLIPLoss()
         self.criterion = nn.CrossEntropyLoss()
 
-        self.classifier = nn.Sequential(nn.ReLU(), nn.Linear(cfg.MODEL.OUTPUT_DIM, cfg.MODEL.OUTPUT_DIM//2),
-                                        nn.ReLU(), nn.Linear(cfg.MODEL.OUTPUT_DIM//2, cfg.MODEL.NUM_CLASSES))
+        # self.classifier = nn.Sequential(nn.ReLU(), nn.Linear(cfg.MODEL.OUTPUT_DIM, cfg.MODEL.OUTPUT_DIM//2),
+        #                                 nn.ReLU(), nn.Linear(cfg.MODEL.OUTPUT_DIM//2, cfg.MODEL.NUM_CLASSES))
+        self.classifier = nn.Sequential(nn.ReLU(), nn.Linear(cfg.MODEL.OUTPUT_DIM, cfg.MODEL.NUM_CLASSES))
 
         self.save_hyperparameters(cfg)
         # self.automatic_optimization = False
@@ -140,6 +141,6 @@ class Learner(pl.LightningModule):
         # optimizer2 = torch.optim.AdamW(self.classifier.parameters(), lr=cfg.SOLVER.BASE_LR2, weight_decay=cfg.SOLVER.WEIGHT_DECAY)
         # scheduler1 = get_linear_schedule_with_warmup(optimizer1, num_warmup_steps=cfg.SOLVER.WARMUP_ITERS, num_training_steps=-1)
         # scheduler2 = get_linear_schedule_with_warmup(optimizer2, num_warmup_steps=cfg.SOLVER.WARMUP_ITERS, num_training_steps=-1)
-        # linear_sched = LinearLR(optimizer1, start_factor=0.01, total_iters=9600)
+        # linear_sched = LinearLR(optimizer1, start_factor=0.05, total_iters=9600)
 
         return [optimizer1], []
